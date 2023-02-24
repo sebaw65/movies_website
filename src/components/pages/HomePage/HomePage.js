f4b2d6784a6e7d30ef972325781dc99ba8d91039
sebaw65 <sebaw65@gmail.com>
2023-02-24 20:23:47 +0100

    getting data from API

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import * as Styles from './HomePage.styles';
 import { useMoviesData } from '../../../hooks/useMoviesData';
 
 const HomePage = () => {
   const { movies, loading, error } = useMoviesData('/popular');
   console.log(movies);
 
-  return (
-    <div>
-      {movies.map((movie) => (
-        <p key={movie.id}>{movie.title}</p>
-      ))}
+  const moviesList = movies.map((movie) => (
+    <div key={movie.id}>
+      <p>{movie.title}</p>
+      <Styles.PosterImg
+        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+      />
+      <p>{movie.release_date.split('-')[0]}</p>
+      <p>{movie.vote_average}</p>
     </div>
+  ));
+
+  return (
+    <Styles.Wrapper>
+      <Styles.StyledMoviesList>{moviesList}</Styles.StyledMoviesList>
+    </Styles.Wrapper>
   );
 };
 
