316297f70d116c307804794b27c759306e429e6d
sebaw65 <sebaw65@gmail.com>
2023-02-26 18:29:32 +0100

    added: display posters, styled in grid, and on hover actions

---
+++ src/components/organisms/MoviePoster/MoviePoster.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import * as Styles from './MoviePoster.styles';
+import { AiFillStar } from 'react-icons/ai';
+
+const Movie = ({ imgUrl, title, relaseDate, rate }) => {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <Styles.MovieContainer
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <Styles.PosterImg
+        src={`https://image.tmdb.org/t/p/w500/${imgUrl}`}
+        alt={`${title} poster`}
+        isHovered={hover}
+      />
+      {hover && (
+        <Styles.MovieDetails>
+          <Styles.TitleAndYear>
+            <Styles.MovieTitle>{title}</Styles.MovieTitle>
+            <Styles.MovieProdYear>
+              {relaseDate.split('-')[0]}
+            </Styles.MovieProdYear>
+          </Styles.TitleAndYear>
+          <Styles.MovieRating>
+            <AiFillStar />
+            <p>{rate}</p>
+          </Styles.MovieRating>
+        </Styles.MovieDetails>
+      )}
+    </Styles.MovieContainer>
+  );
+};
+
+export default Movie;
