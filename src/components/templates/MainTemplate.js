cd7069fdca645ae10c7aa9037cca3187b57a56e5
sebaw65 <sebaw65@gmail.com>
2023-03-03 20:40:11 +0100

    added movie search
    need to fix not reloading page inside /movie/

--- src/components/templates/MainTemplate.js
+++ src/components/templates/MainTemplate.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSearchMovies } from '../../hooks/useSearchMovies';
+import { Link } from 'react-router-dom';
 
 const MainTemplate = ({ children }) => {
+  const [input, setInput] = useState('');
+  const { moviesList } = useSearchMovies(input);
+  console.log(moviesList);
+
+  const handleOnChange = (e) => {
+    setInput((prev) => [e.target.value]);
+  };
   return (
     <div style={{ backgroundColor: '#131313', color: '#fff', height: '100%' }}>
-      {children}
+      <div
+        style={{
+          display: 'flex',
+          height: '4rem',
+          width: '100vw',
+          justifyContent: 'center',
+          paddingTop: '15px',
+          flexFlow: 'row wrap',
+        }}
+      >
+        <input
+          type="text"
+          value={input}
+          placeholder="Wpisz nazwę filmu"
+          style={{
+            width: '90vw',
+            height: '2.5rem',
+            fontSize: '1.8rem',
+            paddingLeft: '10px',
+          }}
+          onChange={handleOnChange}
+        />
+
+        {moviesList
+          ? moviesList.map((el) => (
+              <Link
+                key={el.id}
+                to={`/movie/${el.id}`}
+                style={{
+                  flexBasis: '90%',
+                  width: '0px',
+                  zIndex: '20',
+                  backgroundColor: 'white',
+                  color: 'black',
+                  textDecoration: 'none',
+                }}
+              >
+                <p>{el.original_title}</p>
+              </Link>
+            ))
+          : null}
+      </div>
+      <div style={{ display: 'flex', marginTop: '20px' }}>{children}</div>
     </div>
   );
 };
