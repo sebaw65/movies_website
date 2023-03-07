ef38d8b56936479a2492197403df2902ad7f85d0
sebaw65 <sebaw65@gmail.com>
2023-03-07 18:50:19 +0100

    probably fixed a netlify error

--- src/hooks/useMoviesData.js
+++ src/hooks/useMoviesData.js
@@ -6,28 +6,28 @@ export const useMoviesData = (query) => {
     [error, setError] = useState(null);
 
   useEffect(() => {
+    const getData = async () => {
+      const key = process.env.REACT_APP_API_KEY;
+      try {
+        const res = await fetch(
+          `https://api.themoviedb.org/3/movie/${query}?api_key=${key}&page=1`
+        );
+        if (res.status === 200) {
+          const movies = await res.json();
+          //jesli istnieje klucz results to zwracamy jego zawartosc
+          if (movies.results) setMovies(movies.results);
+          //jesli nie istnieje zwracamy caly obiekt
+          else setMovies(movies);
+        }
+      } catch (err) {
+        setError(err.message);
+      }
+      setLoading(false);
+    };
+
     setLoading(true);
     getData();
   }, [query]);
-
-  const getData = async () => {
-    const key = process.env.REACT_APP_API_KEY;
-    try {
-      const res = await fetch(
-        `https://api.themoviedb.org/3/movie/${query}?api_key=${key}&page=1`
-      );
-      if (res.status === 200) {
-        const movies = await res.json();
-        //jesli istnieje klucz results to zwracamy jego zawartosc
-        if (movies.results) setMovies(movies.results);
-        //jesli nie istnieje zwracamy caly obiekt
-        else setMovies(movies);
-      }
-    } catch (err) {
-      setError(err.message);
-    }
-    setLoading(false);
-  };
 
   return { movies, loading, error };
 };
--- src/hooks/useSearchMovies.js
+++ src/hooks/useSearchMovies.js
@@ -4,21 +4,21 @@ export const useSearchMovies = (props) => {
   const [moviesList, setMoviesList] = useState([]);
 
   useEffect(() => {
+    const searchMovies = async () => {
+      const key = process.env.REACT_APP_API_KEY;
+      const res = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${props}`
+      );
+      const movies = await res.json();
+      setMoviesList(movies.results);
+    };
+
     let debounce = null;
 
     if (props.length === 0) setMoviesList([]);
     debounce = setTimeout(() => searchMovies(), 500);
     return () => clearInterval(debounce);
   }, [props]);
-
-  const searchMovies = async () => {
-    const key = process.env.REACT_APP_API_KEY;
-    const res = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${props}`
-    );
-    const movies = await res.json();
-    setMoviesList(movies.results);
-  };
 
   return {
     moviesList,
